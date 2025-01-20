308ccbd1c9fe5c1ea823bd842a309967945c9f08
Gustavo Oliveira <gustavo.cavalcante.oliveira@live.com>
2025-01-20 11:06:55 -0300

    Saving progress

--- src/clients/__tests__/stormGlass.test.ts
+++ src/clients/__tests__/stormGlass.test.ts
@@ -11,7 +11,6 @@ describe('StormGlass client', () => {
   const lat = -33.792726;
   const lng = 151.289824;
 
-
   it('should return the normalized forecast from the StormGlass service', async () => {
     mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
     const stormGlass = new StormGlass(mockedAxios);
@@ -20,12 +19,26 @@ describe('StormGlass client', () => {
     expect(forecast.length).toBe(3);
   });
 
-  it ('should return filtered normalized forecast from the StormGlass service if not all items are valid', async () => {
-    const stormGlassWeather3HoursFixtureCopy = _.cloneDeep(stormGlassWeather3HoursFixture);
-    stormGlassWeather3HoursFixtureCopy.hours[0].swellDirection.noaa = 0;
-    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixtureCopy });
+  it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
+    const incompleteForecast = {
+      time: 'time',
+      hours: [
+        {
+          swellDirection: {
+            noaa: 0,
+          },
+        },
+      ],
+    };
+    mockedAxios.get.mockResolvedValue({ data: incompleteForecast });
     const stormGlass = new StormGlass(mockedAxios);
     const forecast = await stormGlass.fetchPoints(lat, lng);
-    expect(forecast.length).toBe(2);
-  })
+    expect(forecast).toEqual([]);
+  });
+
+  it('should return filtered normalized forecast from the StormGlass service if not all items are valid', async () => {
+    mockedAxios.get.mockRejectedValue({ message: 'Network Error' });
+    const stormGlass = new StormGlass(mockedAxios);
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow('Error');
+  });
 });
